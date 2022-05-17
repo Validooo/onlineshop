de37033a95646176041b6e16a615d48bca84913e
walid.elkassem99@gmail.com <walid.elkassem99@gmail.com>
2022-05-17 19:56:40 +0200

    MY ONLINE SHOP

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
+import { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Topbar from './Components/Products/Topbar/Topbar';
+import Products from './Components/Products/Products';
+import { commerce } from './commerce';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-function App() {
+
+import React, { Component } from 'react'
+
+const App = () => {
+
+
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
+
+  useEffect(() => {
+    fetchProducts();
+    fetchCart();
+  },
+  )
+
+  //console.log(cart);
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+    setProducts(data);
+  };
+
+
+  const fetchCart = async () => {
+    setCart(await commerce.cart.retrieve());
+  }
+
+  const handleAddToCart = async (productId, quantity) => {
+    const item = await commerce.cart.add(productId, quantity);
+    setCart(item)
+  }
+
+
+  // console.log(cart.total_items + "in app")
+
+
+  //  console.log(this.state.pprod.price.formatted)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+
+      <div >
+
+        <Topbar cart={cart} />
+        <Routes>
+          <Route path="/" element={<Products products={products} handleAddToCart={handleAddToCart} />} />
+
+        </Routes>
+
+
+
+      </div>
+
+    </BrowserRouter>
+  )
+
 }
 
 export default App;
