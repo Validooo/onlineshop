b70ba5305a7009c505519832291c41dbfc1dcd02
walid.elkassem99@gmail.com <walid.elkassem99@gmail.com>
2022-07-28 18:21:10 +0200

    Updates

---
+++ precache-manifest.3c8e524e1f1f512fa3a92ab60bf3cfe3.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5bf492345ee5967ea84",
+    "url": "/onlineshop/static/css/main.adf7dbc2.chunk.css"
+  },
+  {
+    "revision": "d5bf492345ee5967ea84",
+    "url": "/onlineshop/static/js/main.d5bf4923.chunk.js"
+  },
+  {
+    "revision": "2473ae3b2bb4ce736e65",
+    "url": "/onlineshop/static/js/1.2473ae3b.chunk.js"
+  },
+  {
+    "revision": "1cb71859904feba23738",
+    "url": "/onlineshop/static/css/2.8af4b33b.chunk.css"
+  },
+  {
+    "revision": "1cb71859904feba23738",
+    "url": "/onlineshop/static/js/2.1cb71859.chunk.js"
+  },
+  {
+    "revision": "44ff5f88b7436be4f6b6",
+    "url": "/onlineshop/static/js/runtime~main.44ff5f88.js"
+  },
+  {
+    "revision": "dbfb869310ce03e9726ec312fd56ed8d",
+    "url": "/onlineshop/static/media/Toysshopp.dbfb8693.png"
+  },
+  {
+    "revision": "05f9d709c1c4d31fe2d844bf70647a50",
+    "url": "/onlineshop/static/media/kelki.05f9d709.png"
+  },
+  {
+    "revision": "d62c2f54151f3cc17157f85c79e76e5b",
+    "url": "/onlineshop/static/media/watergun.d62c2f54.jpg"
+  },
+  {
+    "revision": "996beb6f3a03395b2487cc2b238cbecd",
+    "url": "/onlineshop/static/media/sommersale.996beb6f.png"
+  },
+  {
+    "revision": "121b9c24ea4f50d3862f3c8d531a5be8",
+    "url": "/onlineshop/static/media/lego.121b9c24.jpg"
+  },
+  {
+    "revision": "3ba1ec151ee2778e0c964a55d61eabf0",
+    "url": "/onlineshop/static/media/rutsch.3ba1ec15.jpg"
+  },
+  {
+    "revision": "4327497d1e0118fd6e8ca315d9e8f482",
+    "url": "/onlineshop/static/media/logosss.4327497d.png"
+  },
+  {
+    "revision": "b28cf34da09cb8a19b34c89d935d046c",
+    "url": "/onlineshop/static/media/sale.b28cf34d.png"
+  },
+  {
+    "revision": "d167c7ac0104f4b666b95024692261c0",
+    "url": "/onlineshop/static/media/everythingforyourchild.d167c7ac.png"
+  },
+  {
+    "revision": "a5002ca2eced418c1278d5cc482ed2ce",
+    "url": "/onlineshop/static/media/besttoys.a5002ca2.png"
+  },
+  {
+    "revision": "45c8f20bf2de10f73e9bfec5c7a089cb",
+    "url": "/onlineshop/static/media/need.45c8f20b.png"
+  },
+  {
+    "revision": "56d715185b35885a2681160d7f40c7a6",
+    "url": "/onlineshop/static/media/logoo.56d71518.png"
+  },
+  {
+    "revision": "1883e5678f1d6f3281848de2662a43c4",
+    "url": "/onlineshop/static/media/white-backgorund.1883e567.png"
+  },
+  {
+    "revision": "02d73af041d124f5fc18700f2a050e08",
+    "url": "/onlineshop/static/media/Spielzeuge.02d73af0.jpg"
+  },
+  {
+    "revision": "27c12fa8aa9d668f0f1a79f505605552",
+    "url": "/onlineshop/static/media/boy-with-girl.27c12fa8.jpg"
+  },
+  {
+    "revision": "57ff4e320f8229fe42783f403a424b9b",
+    "url": "/onlineshop/index.html"
+  }
+];
